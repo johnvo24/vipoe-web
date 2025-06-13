b899f67992aa5ead58ff9624005bca6c329d21e3
chjcfcaloc2020 <nguyentruongchi83@gmail.com>
2025-06-13 14:38:49 +0700

    done VIP-47

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Link from 'next/link'
 import SignUpForm from './sign-up-form'
 
 const SignUp = () => {
@@ -7,12 +6,8 @@ const SignUp = () => {
     <div className='h-screen bg-cover flex justify-center items-center' style={{ backgroundImage: 'url(/images/sign-up-bg.jpg)' }}>
       <div className='w-1/3 flex-col items-center p-8 bg-[#fff] rounded-lg border border-[#d9d9d9]'>
         <p className='text-4xl text-center font-bold mb-8'>VIPOE</p>
-        <p className='text-md text-start font-semibold mb-2'>Create account</p>
+        <p className='text-md text-start font-semibold mb-2'>Create account for Vipoe</p>
         <SignUpForm />
-        <p className='mt-6 text-center'>
-          Do you have an account? 
-          <Link href='/login' className='ms-2 font-semibold hover:underline'>Login</Link>
-        </p>
       </div>
     </div>
   )
--- src/app/(auth)/sign-up/sign-up-form.tsx
+++ src/app/(auth)/sign-up/sign-up-form.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import React from 'react'
+import React, { use, useState } from 'react'
+import Link from 'next/link'
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -11,6 +12,9 @@ import { CircleUser } from 'lucide-react'
 import { Mail } from 'lucide-react'
 import { LockKeyhole } from 'lucide-react'
 import { ALargeSmall } from 'lucide-react'
+import { AxiosError } from 'axios'
+import { api } from '@/lib/utils'
+import { set } from 'zod/v4-mini'
 
 const formSchema = z.object({
   full_name: z.string().max(100),
@@ -26,6 +30,11 @@ const formSchema = z.object({
 type FormValues = z.infer<typeof formSchema>
 
 const SignUpForm = () => {
+  const [emailVerification, setEmailVerification] = useState(false)
+  const [userCache, setUserCache] = useState({
+    full_name: "",
+    email: "",
+  })
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -37,71 +46,91 @@ const SignUpForm = () => {
   })
 
   async function onSubmit(data: FormValues) {
-    console.log("Form submitted with data:", data)
-    // try {
-    //   const response = await api.post("/auth/register", data)
-    //   alert("Registration successful!")
-    // } catch (error) {
-    //   if (error instanceof AxiosError) {
-    //     alert(error.response?.data?.detail || "Registration failed")
-    //   }
-    // }
+    try {
+      // await api.post("/auth/register", data)
+      setUserCache({
+        full_name: data.full_name,
+        email: data.email,
+      })
+      setEmailVerification(true)
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        console.log(error.response?.data?.detail || "Registration failed")
+      }
+    }
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField 
-          control={form.control}
-          name="full_name"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="Enter fullname" icon={<ALargeSmall/>} {...field}/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField 
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="Enter username" icon={<CircleUser/>} {...field}/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField 
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="Enter email" icon={<Mail/>} {...field}/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField 
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input type="password" placeholder="Enter password" icon={<LockKeyhole/>} {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" size={'lg'} className="w-full cursor-pointer">Create account</Button>
-      </form>
-    </Form>
+    <div>
+      {emailVerification ? (
+        <div className="bg-amber-200 rounded-lg py-4 px-6">
+          <p className="text-sm font-normal">
+            Welcome <span className='font-bold'>{userCache.full_name}</span>, your account has been successfully registered. 
+            We have sent you an active email at email <span className='font-bold'>{userCache.email}</span>. Please check your inbox to complete.
+          </p>
+        </div>
+      ) : 
+      <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField 
+              control={form.control}
+              name="full_name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter fullname" icon={<ALargeSmall/>} {...field}/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField 
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter username" icon={<CircleUser/>} {...field}/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField 
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter email" icon={<Mail/>} {...field}/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField 
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input type="password" placeholder="Enter password" icon={<LockKeyhole/>} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" size={'lg'} className="w-full cursor-pointer">Create account</Button>
+          </form>
+        </Form>
+        <p className='mt-6 text-center'>
+          Do you have an account? 
+          <Link href='/login' className='ms-2 font-semibold hover:underline'>Login</Link>
+        </p>
+      </div>
+      }
+    </div>
   )
 }
 
