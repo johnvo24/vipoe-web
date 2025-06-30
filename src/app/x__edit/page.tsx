282fbf094c7fabe1bf64fd1b6ca60860f0a15d11
John Vo <vochidung243@gmail.com>
2025-06-30 18:01:02 +0700

    fix: remove edit page

--- src/app/x__edit/page.tsx
+++ src/app/x__edit/page.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Header from '@/components/layout/Header'
-import Content from './content'
+// import React from 'react'
+// import Header from '@/components/layout/Header'
+// // import Content from './content'
 
-const Edit = () => {
-  return (
-    <div className="bg-transparent">
-      <Header
-        className="fixed top-0 left-0"
-      />
-      <Content />
-    </div>
-  )
-}
+// const Edit = () => {
+//   return (
+//     <div className="bg-transparent">
+//       <Header
+//         className="fixed top-0 left-0"
+//       />
+//       {/* <Content /> */}
+//     </div>
+//   )
+// }
 
-export default Edit
+// export default Edit
